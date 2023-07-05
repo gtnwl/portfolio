b1a4ee9580370ddd28c14e558bec4a783c22d58e
gtnwl <o_o3293@naver.com>
2023-07-05 14:55:22 +0900

    skills page mui, json 사용

--- src/html/skills.js
+++ src/html/skills.js
@@ -1,56 +1,59 @@
 import React from "react";
 import "../resources/sass/common.scss";
 import $ from "jquery";
-function SKILLS() {
+import Grid from "@mui/material/Grid";
+import { Card, CardContent, Typography } from "@mui/material";
+import skillsData from "./skillsData.json";
+
+const SKILLS = () => {
+  const skillData = skillsData.data;
+
   const $container = $(".sub .container");
   $container.addClass("subMovePage");
   return (
     <>
-      <div className="contents index" id="index2">
-        <h2 className="title sub_title">SKILLS</h2>
-        <div className="itemBox itemBox_v2 pT50">
-          <div className="item item_v2">
-            <p className="text pB25">HTML</p>
-            <ul className="textBox gap10">
-              <li className="text text_v5">
-                웹표준 및 웹접근성을 항상 고려하여 마크업을 작성
-              </li>
-              <li className="text text_v5">시멘틱태그 사용 가능</li>
-              <li className="text text_v5">
-                공통으로 사용하는 폼요소 className명 생성하여 효율적인 코딩 가능
-              </li>
-            </ul>
-          </div>
-          <div className="item item_v2">
-            <p className="text pB25">CSS</p>
-            <ul className="textBox gap10">
-              <li className="text text_v5">
-                브라우저 호환성을 고려한 코딩 가능(반응형)
-              </li>
-              <li className="text text_v5">
-                공통으로 사용하는 폼요소 생성하여 효율적인 코딩 가능
-              </li>
-              <li className="text text_v5">SCSS 사용 가능</li>
-            </ul>
-          </div>
-          <div className="item item_v2">
-            <p className="text pB25">JAVASCRIT & JQUERY</p>
-            <ul className="textBox gap10">
-              <li className="text text_v5">
-                JavaScript와 jQuery의 기본 구조를 이해하며 활용 가능
-              </li>
-              <li className="text text_v5">선택자, 오픈소스 활용 가능</li>
-            </ul>
-          </div>
-          <div className="item item_v2">
-            <p className="text pB25">FIGMA</p>
-            <ul className="textBox gap10">
-              <li className="text text_v5">디자이너와 협력하여 툴 사용 가능</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <Grid
+        className="contents index"
+        id="index2"
+        flexDirection="column"
+        sx={{ display: "flex", gap: "50px" }}
+      >
+        <Typography component="h2" variant="div" sx={{ fontSize: "64px" }}>
+          SKILLS
+        </Typography>
+        <Grid container justifyContent="center" sx={{ gap: "20px" }}>
+          {skillData.map((card) => {
+            const id = card.index;
+            const textsArray = Object.values(card.texts[0]);
+            return (
+              <Card key={id} elevation={3} sx={{ flex: 1, minWidth: "200px" }}>
+                <CardContent>
+                  <Typography
+                    variant="b"
+                    component="h3"
+                    pb={1}
+                    sx={{ fontSize: "22px" }}
+                  >
+                    {card.title}
+                  </Typography>
+                  {textsArray.map((item) => {
+                    return (
+                      <Typography
+                        pt={1}
+                        key={item}
+                        sx={{ fontSize: "1.12rem" }}
+                      >
+                        {item}
+                      </Typography>
+                    );
+                  })}
+                </CardContent>
+              </Card>
+            );
+          })}
+        </Grid>
+      </Grid>
     </>
   );
-}
+};
 export default SKILLS;
