35cedd050993a8924b7baf3826ea0c12616ce507
gtnwl <o_o3293@naver.com>
2023-07-25 23:47:03 +0900

    다크모드 작업중,,

---
+++ src/theme/theme.js
@@ -0,0 +1,17 @@
+export const darkTheme = {
+  defaultColor: "#fff",
+  pointColor: "#fff",
+  bgColor: "#000",
+  borderColor: "1px solid #fff",
+};
+export const lightTheme = {
+  defaultColor: "#000",
+  pointColor: "#26498d",
+  bgColor: "#fff",
+  borderColor: "1px solid #231f20",
+};
+
+export const theme = {
+  darkTheme,
+  lightTheme,
+};
