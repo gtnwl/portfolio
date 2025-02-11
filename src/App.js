a1ea8fe60db709398f97847de3461c9d84f1f925
suji <suji@sujiui-MacBookAir.local>
2025-02-11 15:47:30 +0900

    fix: header a tag edit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RESUME from "./html/Resume.js";
 import RESUMEDETIAL from "./html/Resume_detail.js";
-import Acsell from "./html/projects/acsell/Acsell.js";
-import Zikpay from "./html/projects/zikpay/Zikpay.js";
-import Tailorship from "./html/projects/tailorship/Tailorship.js";
-import Sangjo from "./html/projects/sangjo/Sangjo.js";
-import Copyright from "./html/projects/copyright/Copyright.js";
-import Tobemall from "./html/projects/tobemall/Tobemall.js";
-import Babathe from "./html/projects/babathe/Babathe.js";
-import Innocean from "./html/projects/innocean/Innocean.js";
-import Catbox from "./html/projects/catbox/Catbox.js";
-import Bandi from "./html/projects/bandi/Bandi.js";
 import "./resources/sass/common.scss";
 import Header from "./html/include/header.js";
-import SKILLS from "./html/skills.js";
-import ABOUT from "./html/about.js";
-import Footer from "./html/include/footer.js";
 const App = () => {
   return (
     <>
@@ -35,20 +22,7 @@ const App = () => {
                     </>
                   }
                 />
-                {/* <Route path={"/skills"} element={<SKILLS />} />
-                <Route path={"/about"} element={<ABOUT />} />
-                <Route path={"/Bandi"} element={<Bandi />} />
-                <Route path={"/Catbox"} element={<Catbox />} />
-                <Route path={"/Innocean"} element={<Innocean />} />
-                <Route path={"/Babathe"} element={<Babathe />} />
-                <Route path={"/Tobemall"} element={<Tobemall />} />
-                <Route path={"/Copyright"} element={<Copyright />} />
-                <Route path={"/Sangjo"} element={<Sangjo />} />
-                <Route path={"/Tailorship"} element={<Tailorship />} />
-                <Route path={"/Zikpay"} element={<Zikpay />} />
-                <Route path={"/Acsell"} element={<Acsell />} /> */}
               </Routes>
-              <Footer />
             </div>
           </div>
         </div>
